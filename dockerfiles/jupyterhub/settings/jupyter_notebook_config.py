7015181535763596d262413e53d8e75c15733c9d
Driaan <debeste.driaan@gmail.com>
2021-01-22 19:45:13 +0200

    update jupyterhub (customization, extensions)

--- dockerfiles/jupyterhub/settings/jupyter_notebook_config.py
+++ dockerfiles/jupyterhub/settings/jupyter_notebook_config.py
@@ -1,3 +1,14 @@
 import os
 # permission bit 775 for directories
 os.umask(0o002) 
+
+
+c = get_config()
+
+
+# memory
+c.NotebookApp.ResourceUseDisplay.mem_limit = 4 *1024*1024*1024
+
+# cpu
+c.NotebookApp.ResourceUseDisplay.track_cpu_percent = True
+c.NotebookApp.ResourceUseDisplay.cpu_limit = 16
